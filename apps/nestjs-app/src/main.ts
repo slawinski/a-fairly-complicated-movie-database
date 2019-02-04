376527d850b0953ff11b529973dc47c9ef08cf8a
piotr-s <piotr.slawinski1@gmail.com>
2019-02-04 21:06:05 +0100

    Implement swagger

--- apps/nestjs-app/src/main.ts
+++ apps/nestjs-app/src/main.ts
@@ -4,11 +4,20 @@
  **/
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Nest API Example')
+    .setDescription('Przykładowy projekt w Node.js i TypeScript')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3333, () => {
     console.log('Listening at http://localhost:3333');
   });
